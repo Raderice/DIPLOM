9bde12102d15644426a417dc7a02d97a46be307d
Raderice <radericegta@gmail.com>
2026-05-09 22:41:55 +0300

    beta011

---
+++ project/start.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+NGINX_PORT="${PORT:-8080}"
+export NGINX_PORT
+
+envsubst '$NGINX_PORT' < /etc/nginx/templates/render.conf.template > /etc/nginx/conf.d/default.conf
+
+if [ "${RUN_MIGRATIONS:-}" = "true" ]; then
+  npm run prisma:migrate -w @board-games/backend
+fi
+
+PORT=4000
+export PORT
+
+node /app/packages/backend/dist/index.js &
+nginx -g 'daemon off;'
