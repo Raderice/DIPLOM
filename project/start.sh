f7d72cfb186021f6ccb1876e89a5dc5ce6ab8ca7
Raderice <radericegta@gmail.com>
2026-05-09 23:00:55 +0300

    beta012

--- project/start.sh
+++ project/start.sh
@@ -4,6 +4,8 @@ set -e
 NGINX_PORT="${PORT:-8080}"
 export NGINX_PORT
 
+mkdir -p /etc/nginx/conf.d
+
 envsubst '$NGINX_PORT' < /etc/nginx/templates/render.conf.template > /etc/nginx/conf.d/default.conf
 
 if [ "${RUN_MIGRATIONS:-}" = "true" ]; then
